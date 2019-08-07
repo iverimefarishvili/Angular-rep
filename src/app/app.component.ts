fd75a63b5ea5bacfd4fed9d461f51acb30224677
iverimefarishvili <mefarishvili922@gmail.com>
2019-08-07 13:21:13 +0400

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-    h3{
-      color:blue
-    }
-  `]
+  styleUrls: ['./app.component.css']
+ 
 })
 export class AppComponent {
-  title = 'my-app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { Recipe } from './recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,8 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe('test recipe','test','https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/mrtrending0475.jpg?itok=ULkGk3Pn')
   ];
@@ -15,6 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
