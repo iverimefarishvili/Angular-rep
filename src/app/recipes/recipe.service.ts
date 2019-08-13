efdf8693a546f1f2373f80105fbcf077e6044bef
iverimefarishvili <mefarishvili922@gmail.com>
2019-08-13 15:51:24 +0400

    add

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,12 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+
+
 import { Recipe } from './recipe-list/recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService { 
-    recipeSelected = new EventEmitter<Recipe>();
 
     recipes: Recipe[] = [
         new Recipe('test recipe','test','https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/mrtrending0475.jpg?itok=ULkGk3Pn', [ new Ingredient('banana', 1)]),
