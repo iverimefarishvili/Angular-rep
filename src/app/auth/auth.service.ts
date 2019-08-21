bf422c7fa56dbf307ecf12de98035ef2466f9411
iverimefarishvili <mefarishvili922@gmail.com>
2019-08-21 13:11:49 +0400

    add

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -58,6 +58,24 @@ export class AuthService {
           }));
     }
 
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+        const loadedUser = new User(userData.email, userData.id, userData._token, userData._tokenExpirationDate);
+    
+    
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -72,6 +90,7 @@ export class AuthService {
                 token,
                 expirationDate);
                 this.user.next(user);
+                localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(error: HttpErrorResponse) {
