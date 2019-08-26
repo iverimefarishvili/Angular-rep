ab392c04ffb3b89e6109ef51c1691831105d8918
iverimefarishvili <mefarishvili922@gmail.com>
2019-08-26 10:19:34 +0400

    add

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -27,25 +27,28 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   constructor(private slService: ShoppingListService, private store: Store<{shoppingList: {ingredients: Ingredient []}}>) { }
 
   ngOnInit() {
-    this.subscription = this.slService.startedEditing
-      .subscribe(
-        (index: number) => {
-          this.editedItemIndex = index;
+    this.subscription = this.store
+      .select('shoppingList')
+      .subscribe(stateData => {
+        if (stateData.editedIngredientIndex > -1) {
           this.editMode = true;
-          this.editedItem = this.slService.getIngredient(index);
+          this.editedItem = stateData.editedIngredient;
           this.slForm.setValue({
             name: this.editedItem.name,
             amount: this.editedItem.amount
-          })
+          });
+        } else {
+          this.editMode = false;
         }
-      );
+      });
   }
 
   onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
-      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      //this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.store.dispatch(new shoppingListActions.UpdateIngredient({index: this.editedItemIndex, ingredient: newIngredient}))
     } else {
       //this.slService.addIngredient(newIngredient);
       this.store.dispatch(new shoppingListActions.AddIngredient(newIngredient));
@@ -60,7 +63,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onDelete() {
-    this.slService.deleteIngredient(this.editedItemIndex);
+    //this.slService.deleteIngredient(this.editedItemIndex);
+    this.store.dispatch(new shoppingListActions.DeleteIngredient(this.editedItemIndex));
     this.onClear();
   }
 
